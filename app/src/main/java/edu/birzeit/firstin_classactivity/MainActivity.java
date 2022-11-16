1b8b7482e95c196c07ef354a2e3d0ddf89778f17
wael47 <wael.ziada12@gmail.com>
2022-11-16 14:53:03 +0200

    done

--- app/src/main/java/edu/birzeit/firstin_classactivity/MainActivity.java
+++ app/src/main/java/edu/birzeit/firstin_classactivity/MainActivity.java
@@ -3,13 +3,54 @@ package edu.birzeit.firstin_classactivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edtWeight;
+
+    private EditText edtHeight;
+
+    private Switch isMale;
+
+    private Button btnCalculates;
+
+    private TextView txtResult;
+
+    public MainActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupViews();
+    }
 
+    private void setupViews() {
+        edtHeight = findViewById(R.id.edtHeight);
+        edtWeight = findViewById(R.id.edtWeight);
+        isMale = findViewById(R.id.isMale);
+        btnCalculates = findViewById(R.id.btnCalculates);
+        txtResult = findViewById(R.id.txtResult);
+
+        btnCalculates.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double weight = Double.parseDouble(edtWeight.getText().toString());
+                double height = Double.parseDouble(edtHeight.getText().toString());
+                double result = weight/Math.pow(height,2);
+
+                if (isMale.isChecked()){
+                    result += (result * 0.05);
+                }
+                txtResult.setVisibility(View.VISIBLE);
+                txtResult.setText("" + result);
+            }
+        });
     }
 }
