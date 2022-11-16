34c20eddd7d0d6df87f5954d24d77b428cb8c8f7
wael47 <wael.ziada12@gmail.com>
2022-11-16 12:12:20 +0200

    test

---
+++ app/src/main/java/edu/birzeit/firstin_classactivity/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.birzeit.firstin_classactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
